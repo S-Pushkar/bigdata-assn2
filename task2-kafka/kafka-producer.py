e8afab1fccad7a9fd82c4d10126721dc17b6b953
Pushkar S <spushkar0071234@gmail.com>
2024-10-19 21:25:17 +0530

    Added producer file, .gitignore and input and output files

---
+++ task2-kafka/kafka-producer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import kafka
+import sys
+import json
+
+producer = kafka.KafkaProducer(bootstrap_servers='localhost:9092')
+
+consumer1_topic = sys.argv[1]
+
+consumer2_topic = sys.argv[2]
+
+consumer3_topic = sys.argv[3]
+
+for l in sys.stdin:
+    line = l.strip().split(" ")
+
+    if line[0] == 'problem':
+        producer.send(consumer1_topic, json.dumps(line).encode('utf-8'))
+        producer.send(consumer3_topic, json.dumps(line).encode('utf-8'))
+    elif line[0] == 'competition':
+        producer.send(consumer2_topic, json.dumps(line).encode('utf-8'))
+        producer.send(consumer3_topic, json.dumps(line).encode('utf-8'))
+    elif line[0] == 'solution':
+        producer.send(consumer3_topic, json.dumps(line).encode('utf-8'))
+    else:
+        producer.send(consumer1_topic, json.dumps(line).encode('utf-8'))
+        producer.send(consumer2_topic, json.dumps(line).encode('utf-8'))
+        producer.send(consumer3_topic, json.dumps(line).encode('utf-8'))
+
+producer.flush()
