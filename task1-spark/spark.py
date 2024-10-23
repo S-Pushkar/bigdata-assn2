ad7633ec014bf44a838056c655119c76e1b8fe9e
Pushkar S <spushkar0071234@gmail.com>
2024-10-23 15:39:37 +0530

    Done with task 1.2 as well, fingers crossed

--- task1-spark/spark.py
+++ task1-spark/spark.py
@@ -31,16 +31,21 @@ medals = spark.read.csv(sys.argv[5], header=True, inferSchema=True)
 medals = convert_to_uppercase(medals)
 medals.createOrReplaceTempView('medals')
 
+medals = spark.sql('''
+    SELECT * FROM medals
+    WHERE year IN (2012, 2016, 2020)
+''')
+
 output_file = sys.argv[6]
 
 # Task 1.1
 
 athletes = spark.sql('''
-    SELECT DISTINCT * FROM athlete_2012
+    SELECT DISTINCT id, name, sport, country, event, coach_id, '2012' AS year FROM athlete_2012
     UNION
-    (SELECT DISTINCT * FROM athlete_2016
+    (SELECT DISTINCT id, name, sport, country, event, coach_id, '2016' AS year FROM athlete_2016
     UNION
-    SELECT DISTINCT * FROM athlete_2020)
+    SELECT DISTINCT id, name, sport, country, event, coach_id, '2020' AS year FROM athlete_2020)
 ''')
 
 athletes.createOrReplaceTempView('athletes')
@@ -96,12 +101,109 @@ athlete_rank = spark.sql('''
 
 athlete_rank.createOrReplaceTempView('athlete_rank')
 
-spark.sql('''
+result_task_1 = spark.sql('''
     SELECT sport, name, total_score, GOLD, SILVER, BRONZE
     FROM athlete_rank
     WHERE rank = 1
-''').show()
+''')
 
 # Task 1.2
 
+athletes_join_medals.unpersist()
 
+athletes_join_medals = spark.sql('''
+    SELECT a.id AS id, a.name AS name, a.sport AS sport, a.country as athlete_country, m.year AS year, m.medal AS medal, c.id AS coach_id, c.name AS coach_name,
+    CASE
+        WHEN m.medal = 'GOLD' AND m.year = 2012 THEN 20
+        WHEN m.medal = 'SILVER' AND m.year = 2012 THEN 15
+        WHEN m.medal = 'BRONZE' AND m.year = 2012 THEN 10
+        WHEN m.medal = 'GOLD' AND m.year = 2016 THEN 12
+        WHEN m.medal = 'SILVER' AND m.year = 2016 THEN 8
+        WHEN m.medal = 'BRONZE' AND m.year = 2016 THEN 6
+        WHEN m.medal = 'GOLD' AND m.year = 2020 THEN 15
+        WHEN m.medal = 'SILVER' AND m.year = 2020 THEN 12
+        WHEN m.medal = 'BRONZE' AND m.year = 2020 THEN 7
+        ELSE 0
+    END AS score,
+    CASE
+        WHEN m.medal = 'GOLD' THEN 1
+        ELSE 0
+    END AS GOLD,
+    CASE 
+        WHEN m.medal = 'SILVER' THEN 1
+        ELSE 0
+    END AS SILVER,
+    CASE
+        WHEN m.medal = 'BRONZE' THEN 1
+        ELSE 0
+    END AS BRONZE
+    FROM athletes a, medals m, coaches c
+    WHERE a.id = m.id AND a.sport = m.sport AND a.coach_id = c.id AND a.sport = c.sport AND a.country IN ('USA', 'CHINA', 'INDIA') AND a.event = m.event AND a.year = m.year
+''')
+
+athletes_join_medals.createOrReplaceTempView('athletes_join_medals')
+
+athletes_coaches_china = spark.sql('''
+    SELECT * FROM athletes_join_medals
+    WHERE athlete_country = 'CHINA'
+''')
+
+athletes_coaches_china.createOrReplaceTempView('athletes_coaches_china')
+
+athletes_coaches_india = spark.sql('''
+    SELECT * FROM athletes_join_medals
+    WHERE athlete_country = 'INDIA'
+''')
+
+athletes_coaches_india.createOrReplaceTempView('athletes_coaches_india')
+
+athletes_coaches_usa = spark.sql('''
+    SELECT * FROM athletes_join_medals
+    WHERE athlete_country = 'USA'
+''')
+
+athletes_coaches_usa.createOrReplaceTempView('athletes_coaches_usa')
+
+top_5_coaches_china = spark.sql('''
+    SELECT coach_id, coach_name, SUM(score) AS total_score, SUM(GOLD) AS GOLD, SUM(SILVER) AS SILVER, SUM(BRONZE) AS BRONZE
+    FROM athletes_coaches_china
+    GROUP BY coach_id, coach_name
+    ORDER BY total_score DESC, GOLD DESC, SILVER DESC, BRONZE DESC, coach_name ASC
+    LIMIT 5
+''')
+
+top_5_coaches_china.createOrReplaceTempView('top_5_coaches_china')
+
+# top_5_coaches_china.show()
+
+top_5_coaches_india = spark.sql('''
+    SELECT coach_id, coach_name, SUM(score) AS total_score, SUM(GOLD) AS GOLD, SUM(SILVER) AS SILVER, SUM(BRONZE) AS BRONZE
+    FROM athletes_coaches_india
+    GROUP BY coach_id, coach_name
+    ORDER BY total_score DESC, GOLD DESC, SILVER DESC, BRONZE DESC, coach_name ASC
+    LIMIT 5
+''')
+
+top_5_coaches_india.createOrReplaceTempView('top_5_coaches_india')
+
+# top_5_coaches_india.show()
+
+top_5_coaches_usa = spark.sql('''
+    SELECT coach_id, coach_name, SUM(score) AS total_score, SUM(GOLD) AS GOLD, SUM(SILVER) AS SILVER, SUM(BRONZE) AS BRONZE
+    FROM athletes_coaches_usa
+    GROUP BY coach_id, coach_name
+    ORDER BY total_score DESC, GOLD DESC, SILVER DESC, BRONZE DESC, coach_name ASC
+    LIMIT 5
+''')
+
+top_5_coaches_usa.createOrReplaceTempView('top_5_coaches_usa')
+
+# top_5_coaches_usa.show()
+
+result_task_1 = [row.name for row in result_task_1.collect()]
+
+result_task_2 = [row.coach_name for row in top_5_coaches_china.collect()] + [row.coach_name for row in top_5_coaches_india.collect()] + [row.coach_name for row in top_5_coaches_usa.collect()]
+
+with open(output_file, 'w') as f:
+    result = (result_task_1, result_task_2)
+    f.write(str(result))
